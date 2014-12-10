1a62dc4bfda91d4a271e16b657a2213c1ee7fa03
Michael Dang <DangMH@gmail.com>
2014-12-09 22:00:48 -0800

    Added missing ClimbingScore.cs

---
+++ ClimbRecordingApp/ClimbingScore.cs
@@ -0,0 +1,22 @@
+﻿using ClimbRecordingApp.BusinessLayer.Contracts;
+using ClimbRecordingApp.DataLayer.SQLite;
+
+namespace ClimbRecordingApp.BusinessLayer
+{
+    public class ClimbingScore : IBusinessEntity
+    {
+        public const int MAX_GRADE_FORMAT_LENGTH = 10;
+
+        public ClimbingScore()
+        {
+
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int UNIVERSAL_SCORE { get; set; }
+        public int GRADE_FORMAT_ID { get; set; }
+        [MaxLength(MAX_GRADE_FORMAT_LENGTH)]
+        public string GRADE_FORMAT_STRING { get; set; }
+    }
+}
